600a48af2cdc1d977fd1c9adce40dd8eedc7c7e8
Christoph Bünte <info@christophbuente.de>
2013-04-22 16:09:22 +0200

    Use generstor to include select2 stylesheet.

--- lib/generators/spree_bank_transfer/install/install_generator.rb
+++ lib/generators/spree_bank_transfer/install/install_generator.rb
@@ -1,6 +1,15 @@
 module SpreeBankTransfer
   module Generators
     class InstallGenerator < Rails::Generators::Base
+
+      def add_javascripts
+        append_file 'app/assets/javascripts/store/all.js', "//= require select2\n"
+      end
+
+      def add_stylesheets
+        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require select2\n", :before => /\*\//, :verbose => true
+      end
+
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_bank_transfer'
       end
